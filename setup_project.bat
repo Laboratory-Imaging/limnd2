a154a4bcf77b458923366c805db7ebcbb483c341
LJatLIM <lukas.jirusek@lim.cz>
2024-11-12 10:55:00 +0100

    Added install script

---
+++ setup_project.bat
@@ -0,0 +1,8 @@
+git clone https://github.com/Laboratory-Imaging/limnd2.git
+cd limnd2
+python -m venv env
+env\Scripts\activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install build setuptools twine
+pip install --editable .
